d7cfa8a8e87971706e48aa6a89e65dc8ef6d3f9c
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-06-02 18:35:50 -0600

    [:recycle: refactor] public and private routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const cors = require("cors")
 const express = require("express")
+const verifyToken = require("./shared/helpers/verifyToken.js")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/api",require("./routes/index.js")) 
+app.use("/private", verifyToken) 
+app.use("/private",require("./routes/private")) 
+
+app.use("/public",require("./routes/public")) 
+
+
 
 module.exports = app
---
+++ src/shared/helpers/verifyToken.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+
+  if (typeof bearerHeader !== undefined && typeof bearerHeader === 'string') {
+    const token = bearerHeader.split(' ')[1]; 
+    console.log(token)
+
+    jwt.verify(token, "usercred", (error, authData) => {
+      if (error) {
+        res.status(403).json({ error: 'Token inválido' }); 
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(403).json({ error: 'Token no proporcionado' }); 
+  }
+};
+
+module.exports = verifyToken
